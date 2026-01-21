fd5a87f60042675e63217844ab5e26a47e6aa9f7
monacelian <47246070+monacelian@users.noreply.github.com>
2026-01-20 23:38:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,14 @@
 from flask import Flask, jsonify
-import psycopg2
 import os
 
 app = Flask(__name__)
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
 @app.route("/")
 def home():
-    return "Servidor OK"
+    return "Servidor Flask OK"
 
 @app.route("/teste_db")
 def teste_db():
-    try:
-        conn = psycopg2.connect(
-            DATABASE_URL,
-            sslmode="require"
-        )
-        cur = conn.cursor()
+    return jsonify({"status": "rota ok"})
 
-        cur.execute("""
-            INSERT INTO pagamentos (payment_id, email, status, valor)
-            VALUES (%s, %s, %s, %s)
-        """, (
-            "TESTE123",
-            "teste@exemplo.com",
-            "pending",
-            1.00
-        ))
-
-        conn.commit()
-        cur.close()
-        conn.close()
-
-        return jsonify({"status": "ok", "msg": "Registro inserido"})
-
-    except Exception as e:
-        return jsonify({"erro": str(e)}), 500
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+# NÃO use app.run no Railway
